6704ed34bd8dacdbc4b3afca8516a5437e702aa7
royfini <138002571+royfini@users.noreply.github.com>
2023-07-07 16:09:02 +0300

    first commit

---
+++ index1.js
@@ -0,0 +1,26 @@
+let counter = 1;
+
+function getall() {
+    fetch('https://api-e7by.onrender.com/stu')
+        .then(response => response.json())
+        .then(json =>
+            json.forEach(stu => {
+                let students = document.getElementById("ol");
+                let student = document.createElement("li");
+                student.innerHTML = stu.name;
+                students.appendChild(student);
+            })
+        )
+}
+
+
+btn = document.getElementById("btn");
+
+btn.addEventListener('click',dosome)
+
+function dosome()
+{
+    btn.disabled = false;
+    getall();
+    btn.disabled = true;
+}
